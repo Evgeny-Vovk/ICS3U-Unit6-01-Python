b352bb7c99720e148ca4e4eda667670594a2a0c0
Evgeny Vovk <Evgeny.vovk@mths.ca>
2023-01-21 17:22:19 +0000

    done

---
+++ ICS3U-Unit6-01.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2022 Evgeny Vovk All rights reserved.
+#
+# Created by: Evgeny Vovk
+# Created on: January 2022
+# ICS3U-Unit6-01.py File, learning functions with parameters in python.
+
+import random
+
+
+def main():
+
+    sum = 0
+    random_numbers = []
+
+    for counter in range(10):
+        number = random.randint(0, 100)
+        random_numbers.append(number)
+        print("Generated number: {0}".format(number))
+
+    for counter in range(10):
+        sum += random_numbers[counter]
+        average = sum / len(random_numbers)
+
+    print("\nThe average of all numbers is {0}.".format(average))
+    print("")
+
+    print("\n\nDone.")
+
+
+if __name__ == "__main__":
+    main()
